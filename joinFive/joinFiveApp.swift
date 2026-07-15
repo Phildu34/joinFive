2f8ddf333f9575b4f1dcb1a0a33829b612da2530
PhilG <pgreze@gmail.com>
2026-07-15 09:12:30 +0200

    Initial Commit

---
+++ joinFive/joinFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  joinFiveApp.swift
+//  joinFive
+//
+//  Created by Philippe GREZE on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct joinFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
